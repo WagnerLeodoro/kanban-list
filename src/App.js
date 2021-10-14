1616e821d5bab91fb2e1a3b26e8bffeabfce2f3b
WagnerLeodoro <wagner.augustoleodoro@gmail.com>
2021-10-14 01:00:52 -0300

    kanban-tasks

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import GlobalStyles from './styles/global';
+import Header from "./components/Header";
+import Board from "./components/Board";
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <DndProvider backend={HTML5Backend}>
+      <Header />
+      <Board />
+      <GlobalStyles />
+    </DndProvider>
   );
 }
 
---
+++ src/components/Header/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Container } from './styles';
+
+export default function Header() {
+    return (
+        <Container>
+            <h1>Kanban List</h1>
+        </Container>
+    )
+}
